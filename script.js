b922d5c5d0b08c8ef4b80740c93f8e3687ceaf26
rupali <36826017+rupalimo@users.noreply.github.com>
2024-07-17 12:32:56 -0400

    adjust styling

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const showBio = document.getElementById('bio');
+const artistName = document.getElementById('artist');
 function displayBio() {
-	showBio.classList.toggle('hide');
+  showBio.classList.toggle('hide');
+  artistName.classList.toggle('highlight');
 }
