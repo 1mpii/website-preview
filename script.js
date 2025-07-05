c54a907af2574a494ae12047b6302c90ca8bed78
rupali <36826017+rupalimo@users.noreply.github.com>
2025-07-05 15:12:23 -0400

    update images, layout styling, mobile

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const showBio = document.getElementById('bio');
-const artistName = document.getElementById('artist');
-function displayBio() {
-  showBio.classList.toggle('hide');
-  artistName.classList.toggle('highlight');
+function openTab(evt, tabName) {
+	// Declare all variables
+	var i, tabcontent, tabitem;
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName('tabcontent');
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none';
+	}
+
+	// Get all elements with class="tabitems" and remove the class "active"
+	tabitem = document.getElementsByClassName('tabitem');
+	for (i = 0; i < tabitem.length; i++) {
+		tabitem[i].className = tabitem[i].className.replace(' active', '');
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = 'block';
+	evt.currentTarget.className += ' active';
 }
